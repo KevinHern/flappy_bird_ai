061029bc3203a4de5157935ff0c723955431dae2
Kevin Hernandez <hernandez.kevin@galileo.edu>
2022-09-22 18:07:13 -0600

    Small optimization: Closest pipe no longer is a function called every frame

--- models/bird.py
+++ models/bird.py
@@ -20,8 +20,10 @@ class Bird:
     def __init__(
             self,
             starting_position,
+            bird_diameter,
             max_height,
-            total_pipes
+            total_pipes,
+            closest_pipe
     ):
         # Position Vectors
         self.starting_position = starting_position
@@ -36,7 +38,8 @@ class Bird:
         self.flap_velocity = np.array([0, -10])
 
         # Bird constants
-        self.bird_diameter = 35
+        self.bird_diameter = bird_diameter
+        self.closest_pipe = closest_pipe
 
         # Flags
         self.game_over = False
@@ -50,16 +53,16 @@ class Bird:
     def flap(self):
         self.velocity = self.flap_velocity
 
-    def check_collision(self, dual_pipe):
+    def check_collision(self):
         # Get points
         points = np.array([
             # X points
-            [dual_pipe.top_pipe.position[0] - self.bird_diameter / 2,
-             dual_pipe.top_pipe.position[0] + dual_pipe.top_pipe.width + self.bird_diameter / 2],
+            [self.closest_pipe.top_pipe.position[0] - self.bird_diameter / 2,
+             self.closest_pipe.top_pipe.position[0] + self.closest_pipe.top_pipe.width + self.bird_diameter / 2],
             # Top Pipe Y Height
-            [dual_pipe.top_pipe.position[1] + dual_pipe.top_pipe.height + self.bird_diameter / 2],
+            [self.closest_pipe.top_pipe.position[1] + self.closest_pipe.top_pipe.height + self.bird_diameter / 2],
             # Bottom Pipe Y Height
-            [dual_pipe.bottom_pipe.position[1] - self.bird_diameter / 2],
+            [self.closest_pipe.bottom_pipe.position[1] - self.bird_diameter / 2],
         ])
 
         # Check collision Top Pipe
@@ -67,17 +70,15 @@ class Bird:
             if points[1] >= self.position[1] or points[2] <= self.position[1]:
                 self.game_over = True
 
-    def increase_pipe_score(self, passed_pipe):
+    def increase_pipe_score(self, passed_pipe, closest_pipe):
         if passed_pipe and not self.game_over:
             self.pipes_passed += 1
             print("Distance: {}\nPipe Score: {}".format(self.distance, self.pipes_passed))
+            self.closest_pipe = closest_pipe
             self.game_over = self.pipes_passed == self.total_pipes
 
     def draw(self, track):
-        # Check closest pipe first
-        closest_pipe = get_closest_pipe(track=track)
-
-        if closest_pipe is not None:
+        if self.closest_pipe is not None:
             # Proceed normally
             # Calculate new velocity
             self.velocity = np.add(self.velocity, Bird.GRAVITY)
@@ -88,7 +89,7 @@ class Bird:
                 self.position[1] = self.max_height
 
             # Check Collisions
-            self.check_collision(dual_pipe=closest_pipe)
+            self.check_collision()
 
             # Draw
             circle(self.position[0], self.position[1], self.bird_diameter)
@@ -96,7 +97,7 @@ class Bird:
             # Increase distance score
             self.distance += 1
 
-    def reset(self):
+    def reset(self, closest_pipe):
         # Resetting vectors
         self.position = self.starting_position
         self.velocity = np.empty(2)
@@ -108,3 +109,6 @@ class Bird:
 
         # Setting flags back to normal
         self.game_over = False
+
+        # Set new closest pipe
+        self.closest_pipe = closest_pipe
